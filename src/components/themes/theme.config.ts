d24d735038b15029819b7926791be1db33a9b2c7
root <root@srv1477012.hstgr.cloud>
2026-03-26 22:29:00 +0000

    feat: apply C3 branding theme

--- src/components/themes/theme.config.ts
+++ src/components/themes/theme.config.ts
@@ -2,9 +2,13 @@
  * Default theme that loads when no user preference is set
  * Change this value to set a different default theme
  */
-export const DEFAULT_THEME = 'vercel';
+export const DEFAULT_THEME = 'c3';
 
 export const THEMES = [
+  {
+    name: 'C3 Marketing',
+    value: 'c3'
+  },
   {
     name: 'Claude',
     value: 'claude'
